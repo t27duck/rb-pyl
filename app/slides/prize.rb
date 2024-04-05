6c48dad3d270429a1eb4dccfae3459a2e7bd3a23
Tony Drake <t27duck@gmail.com>
2024-04-05 18:42:34 -0400

    Display prize name in slide

--- app/slides/prize.rb
+++ app/slides/prize.rb
@@ -1,4 +1,6 @@
 class Slide::Prize < Slide
+  FONT_SIZE = -3
+
   def text
     "#{@text} worth $#{@cash_value}"
   end
@@ -8,37 +10,153 @@ class Slide::Prize < Slide
   end
 
   def draw_label
-    @game.outputs.labels << {
-      x: @space.x + (Space::WIDTH / 2) + TEXT_SHADOW_OFFSET,
-      y: @space.y + (Space::WIDTH / 2) - TEXT_SHADOW_OFFSET,
-      text: "Prize",
-      alignment_enum: 1,
-      vertical_alignment_enum: 1,
-      font: FONT,
-      size_enum: -1
-    }.merge(COLOR_FONT_SHADOW)
-
-    @game.outputs.labels << {
-      x: @space.x + (Space::WIDTH / 2),
-      y: @space.y + (Space::WIDTH / 2),
-      text: "Prize",
-      alignment_enum: 1,
-      vertical_alignment_enum: 1,
-      font: FONT,
-      size_enum: -1
-    }.merge(COLOR_FONT)
+    send("draw_label#{@text_rows}")
   end
 
   def cycle_prize
     data = @game.state.prize_pool.pick
     @prefix = data[:prefix]
     @text = @prefix ? "#{@prefix} #{data[:text]}" : data[:text]
+    @basic_text = data[:text]
     @cash_value = data[:value]
+    setup_text_info
   end
 
   private
 
   def after_init_hook
     cycle_prize
+  end
+
+  def setup_text_info
+    @text_array = @basic_text.split(" ")
+    @text_array = [@text_array[0], @text_array[1], @text_array[2..].join(" ")] if @text_array.size > 3
+    @text_rows = @text_array.size
+  end
+
+  def draw_label1
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2) + TEXT_SHADOW_OFFSET,
+      y: @space.y + (Space::WIDTH / 2) - TEXT_SHADOW_OFFSET,
+      text: @text_array[0],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT_SHADOW)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2),
+      y: @space.y + (Space::WIDTH / 2),
+      text: @text_array[0],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT)
+  end
+
+  def draw_label2
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2) + TEXT_SHADOW_OFFSET,
+      y: @space.y + (Space::WIDTH / 2) - TEXT_SHADOW_OFFSET + 12,
+      text: @text_array[0],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT_SHADOW)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2),
+      y: @space.y + (Space::WIDTH / 2) + 12,
+      text: @text_array[0],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2) + TEXT_SHADOW_OFFSET,
+      y: @space.y + (Space::WIDTH / 2) - TEXT_SHADOW_OFFSET - 12,
+      text: @text_array[1],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT_SHADOW)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2),
+      y: @space.y + (Space::WIDTH / 2) - 12,
+      text: @text_array[1],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT)
+  end
+
+  def draw_label3
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2) + TEXT_SHADOW_OFFSET,
+      y: @space.y + (Space::WIDTH / 2) - TEXT_SHADOW_OFFSET + 20,
+      text: @text_array[0],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT_SHADOW)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2),
+      y: @space.y + (Space::WIDTH / 2) + 20,
+      text: @text_array[0],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2) + TEXT_SHADOW_OFFSET,
+      y: @space.y + (Space::WIDTH / 2) - TEXT_SHADOW_OFFSET,
+      text: @text_array[1],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT_SHADOW)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2),
+      y: @space.y + (Space::WIDTH / 2),
+      text: @text_array[1],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2) + TEXT_SHADOW_OFFSET,
+      y: @space.y + (Space::WIDTH / 2) - TEXT_SHADOW_OFFSET - 20,
+      text: @text_array[2],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT_SHADOW)
+
+    @game.outputs.labels << {
+      x: @space.x + (Space::WIDTH / 2),
+      y: @space.y + (Space::WIDTH / 2) - 20,
+      text: @text_array[2],
+      alignment_enum: 1,
+      vertical_alignment_enum: 1,
+      font: FONT,
+      size_enum: FONT_SIZE
+    }.merge(COLOR_FONT)
   end
 end
